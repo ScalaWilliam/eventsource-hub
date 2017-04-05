51ae689d3a8bd10b9aa3c3c2f2f4389b48604cc4
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-04-05 20:04:05 +0800

    Fixes #5: Store historical events and allow their retrieval.

--- app/controllers/Main.scala
+++ app/controllers/Main.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.nio.file.{Files, Path, Paths, StandardOpenOption}
 import java.time.Instant
 import javax.inject._
 
@@ -11,19 +12,27 @@ import play.api.libs.iteratee.Concurrent
 import play.api.libs.iteratee.streams.IterateeStreams
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
+
 /**
   * Forwards events from postChannel to aChannel via pushEvents.
   * Singleton needed to keep 'pushEvents' constant.
   */
 @Singleton
-class Main @Inject()(components: ControllerComponents)
-    extends AbstractController(components) {
+class Main(eventsPath: Path)(implicit components: ControllerComponents,
+                             executionContext: ExecutionContext)
+    extends AbstractController(components) { main =>
+
+  @Inject
+  def this()(implicit components: ControllerComponents,
+             executionContext: ExecutionContext) =
+    this(Paths.get(Main.DefaultFile))
 
   def aChannel() = Action { request: Request[AnyContent] =>
     if (request.acceptedTypes.exists(_.toString() == MimeTypes.EVENT_STREAM)) {
       Ok.chunked(content = pushEvents.merge(Main.keepAliveEventSource))
         .as(ContentTypes.EVENT_STREAM)
-    } else Ok("").as(ContentTypes.withCharset(Main.TsvMimeType))
+    } else Ok.sendPath(content = eventsPath)
   }
 
   private val (enumerator, channel) = Concurrent.broadcast[Event]
@@ -31,20 +40,41 @@ class Main @Inject()(components: ControllerComponents)
   private def pushEvents =
     Source.fromPublisher(IterateeStreams.enumeratorToPublisher(enumerator))
 
-  def postChannel() = Action(parse.tolerantText) { request: Request[String] =>
-    val id = Instant.now().toString
-    val event = Event(
-      name = request.getQueryString("event"),
-      data = request.body,
-      id = Some(id)
-    )
-    channel.push(event)
-    Created(id)
+  Files.createDirectories(eventsPath.getParent)
+
+  /** Use approach from https://gist.github.com/ScalaWilliam/37c4ef3c41e656a6d3c02d992f5c6191 **/
+  private val raf =
+    new java.io.RandomAccessFile(eventsPath.toFile, "rw").getChannel
+
+  def appendLine(line: String): Unit = {
+    val lock = raf.lock()
+    val lineBytes = (line + "\n").getBytes("UTF-8")
+    try Files.write(eventsPath, lineBytes, StandardOpenOption.APPEND)
+    finally lock.release()
+  }
+
+  def postChannel(): Action[String] = Action(parse.tolerantText) {
+    request: Request[String] =>
+      val id = Instant.now().toString
+      val event = Event(
+        name = request.getQueryString(Main.EventQueryParameterName),
+        data = request.body,
+        id = Some(id)
+      )
+      val eventLine =
+        s"${event.id.getOrElse("")}\t${event.name.getOrElse("")}\t${event.data}"
+      appendLine(eventLine)
+      channel.push(event)
+      Created(id)
   }
 
 }
 
 object Main {
+
+  val DefaultFile = "events/aChannel.log"
+
+  val EventQueryParameterName = "event"
 
   val TsvMimeType = "text/tab-separated-values"
 
